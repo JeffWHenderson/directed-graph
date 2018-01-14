eb0ff42786d07501f5574363de81451fc57f829a
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-01-14 02:23:37 -0500

    seperated main() into the HelloTestDriver class.

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+  String name;
+  public void greeting() {
+
+    System.out.println("Hello World");
+  }
+}
---
+++ HelloTestDrive.java
@@ -0,0 +1,8 @@
+public class HelloTestDrive{
+
+  public static void main(String[] args){
+    Hello hiya = new Hello();
+    hiya.name = "basic coding";
+    hiya.greeting();
+  }
+}
