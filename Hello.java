833746994ae6b4f9ea87a814a41f256751002625
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-01-14 14:11:56 -0500

    made some comments, compiled code so classes worked together (i'm still new to java). Now main function is working from the TestDrive class.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 
   String name;
+  // got to keep that well encapsulated. Don't expose your variables!!!
+
   public void greeting() {
 
     System.out.println("Hello World");
--- HelloTestDrive.java
+++ HelloTestDrive.java
@@ -4,5 +4,10 @@ public class HelloTestDrive{
     Hello hiya = new Hello();
     hiya.name = "basic coding";
     hiya.greeting();
+    //System.out.println("hello");
   }
 }
+
+/*
+  What about starting the node object with an acive boolean variable.. and if it has a start or and end node the variable can be switched to true.  Would that make it possible to set one string of AB5, BC4 to edge and create nodes based on those????j
+*/
