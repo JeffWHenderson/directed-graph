2dfe9cf76bc46987249905b46577701664127535
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-01-15 12:58:58 -0500

    added all edges to GraphTestDrive.

--- GraphTestDrive.java
+++ GraphTestDrive.java
@@ -1,5 +1,7 @@
 public class GraphTestDrive{
   public static void main(String[] args){
+
+      //AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
     Node nodeA = new Node("A");
     Node nodeB = new Node("B");
     Node nodeC = new Node("C");
@@ -7,7 +9,15 @@ public class GraphTestDrive{
     Node nodeE = new Node("E");
 
     Edge ab5 = new Edge(nodeA, nodeB, 5);
+    Edge bc4 = new Edge(nodeB, nodeC, 4);
+    Edge cd8 = new Edge(nodeC, nodeD, 8);
+    Edge dc8 = new Edge(nodeD, nodeC, 8);
+    Edge de6 = new Edge(nodeD, nodeE, 6);
+    Edge ad5 = new Edge(nodeA, nodeD, 5);
+    Edge ce2 = new Edge(nodeC, nodeE, 2);
+    Edge eb3 = new Edge(nodeE, nodeB, 3);
+    Edge ae7 = new Edge(nodeA, nodeE, 7);
 
-    System.out.println("the edge is " + ab5.start.name + ab5.stop.name + ab5.distance);
+    System.out.println("the edge is " + ae7.start.name + ae7.stop.name + ae7.distance);
   }
 }
